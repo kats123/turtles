78276b5539492300c5c941bbb79c0daf9910a79e
Katherine Sweeney <katherinesweeney777@gmail.com>
2017-12-25 19:00:05 -0500

    first commit

---
+++ turtles.py
@@ -0,0 +1,39 @@
+therine Sweeney
+#December 25th
+#04.02
+
+import turtle
+def main():
+
+  imageDecision = input("What image would you like to draw? A house or a flower?")
+  
+  if(imageDecision == "house"):
+    houseColor = input("What color would you like your house? red or brown?")
+  elif(imageDecision == "flower"):
+    flowerColor = input("what color would you like your flower? pink or blue?")
+  
+  if(imageDecision == "house" and houseColor == "red"):
+    redTurtle = turtle.Turtle()
+    redTurtle.color("red")
+    redTurtle.hideturtle()
+    redTurtle.penup()
+    redTurtle.setpos(-50,-50)
+    redTurtle.pendown()
+    redTurtle.forward(100)
+    redTurtle.left(90)
+    redTurtle.forward(100)
+    redTurtle.left(30)
+    redTurtle.forward(100)
+    redTurtle.left(120)
+    redTurtle.forward(100)
+    redTurtle.left(30)
+    redTurtle.forward(100)
+  elif(imageDecision == "house" and houseColor == "brown"):
+    brownTurtle = turtle.Turtle
+    
+    
+  
+    
+  
+  
+main()
